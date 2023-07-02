df252a3cdca8011b56e2012d0ff7702dffe6a2e7
OlaAbdallah1 <olaabdallah2014@gmail.com>
2023-07-02 03:24:55 +0300

    BlackJack Game

---
+++ index.js
@@ -0,0 +1,55 @@
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+let messageEl = document.getElementById("message");
+let sumEl = document.getElementById("sum");
+let cardsEl = document.getElementById("cards");
+
+function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+
+function renderGame() {
+  sumEl.textContent = "Sum: " + sum;
+  //   cardsEl.textContent = "Cards: " + cards;
+
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  let card = getRandomCard();
+  sum += card;
+  cards.push(card);
+  renderGame();
+}
+
+function getRandomCard() {
+  let randomNumer = Math.floor(Math.random() * 13) + 1;
+  if (randomNumer > 10) {
+    return 10;
+  } else if (randomNumer === 1) {
+    return 11;
+  } else {
+    return randomNumer;
+  }
+}
